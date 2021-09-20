f594958f12427548e9417e55905a5cd54ba0bac2
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2021-09-20 07:47:15 +0300

    Instnace Test

---
+++ src/main/com/doitintl/blaster/test/tests/BucketTest.kt
@@ -0,0 +1,28 @@
+package com.doitintl.blaster.test.tests
+
+
+import com.doitintl.blaster.shared.runCommand
+import com.doitintl.blaster.test.TestBase
+
+class BucketTest(project: String) : TestBase(project) {
+
+    override fun assetTypeIds(): List<String> = listOf("storage.googleapis.com/Bucket")
+
+    override fun createAssets(sfx: String, project: String): List<String> {
+        fun makeBucket(project: String, location: String, bucketName: String) {
+            runCommand("gsutil mb -p $project -l $location gs://$bucketName")
+            println("Created $bucketName")
+        }
+
+
+        val multiregionAsset = assetName("bucket-multiregion")
+        val regionAsset = assetName("bucket-region")
+        makeBucket(project, "us", multiregionAsset)
+        makeBucket(project, "us-central1", regionAsset)
+        return listOf(multiregionAsset, regionAsset)
+    }
+
+
+}
+
+
