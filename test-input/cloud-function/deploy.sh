8f0fe607ed77e25b305e1b55143d22332c3440d9
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2021-10-03 17:39:16 +0300

    Test Cloud Functions

--- test-input/cloud-function/deploy.sh
+++ test-input/cloud-function/deploy.sh
@@ -2,13 +2,12 @@
 
 REGION=us-east1
 
-
 PROJECT=$1
 if [[ -z "$PROJECT" ]]; then
   echo "Must provide project as first param"
   exit 1
 fi
-``
+$()
 FUNC_NAME=$2
 if [[ -z "$FUNC_NAME" ]]; then
   echo "Must provide function name as second param"
